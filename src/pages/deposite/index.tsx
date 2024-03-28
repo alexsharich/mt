a3c261442a7b405c6b23603f7158929156b0b72d
alexsharich <alexsharich@gmail.com>
2024-03-28 23:42:08 +0300

    feat: add lineChart

--- src/pages/deposite/index.tsx
+++ src/pages/deposite/index.tsx
@@ -1,7 +1,9 @@
+import CoffeePriceChart from '@/Components/Coffe/Coffe'
+import MyLineChart from '@/Components/Coffe/Coffe'
 import { getLayoutWithSidebar } from '@/Components/WithSideBarLayout/WithSideBarLayout'
 
 const Deposite = () => {
-  return <div>Deposite</div>
+  return <MyLineChart />
 }
 
 Deposite.getLayout = getLayoutWithSidebar
